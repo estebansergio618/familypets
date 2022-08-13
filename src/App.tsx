d278e8b3d25c2f0d65e53f7c86dea4d84aa83c14
Matias Solis <solisma42@gmail.com>
2022-08-13 19:17:10 -0300

    Added hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hero from './components/Hero';
 import Navigation from './components/Navigation';
 import "./scss/main.scss"
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import background from "../images/puppies-wallpaper.jpg"
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <button className='hero__button hero__button-left'>CONTACTO</button>
+      <button className='hero__button'>AGENDA TU CITA</button>
+    </section>
+  )
+}
+
+export default Hero
