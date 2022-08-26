872d480b215c5099df8c0e117847e1eb2a40da8e
Matias Solis <solisma42@gmail.com>
2022-08-25 23:28:23 -0300

    added modal to services cards

---
+++ src/components/ServicesModal.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { CardInterface } from "../types";
+interface Props {
+  card: CardInterface;
+  setOpenModal: (value: boolean) => void;
+}
+export const ServicesModal = ({ card, setOpenModal }: Props) => {
+  return (
+    <div className="servicesmodal__background">
+      <div className="servicesmodal">
+        <div className="servicesmodal__nav">
+          <span
+            className="servicesmodal__icon material-symbols-outlined"
+            style={{ color: card.color }}
+          >
+            {card.icon}
+          </span>
+          <p className="servicesmodal__title">{card.title}</p>
+          <span
+            className="servicesmodal__icon servicesmodal__icon-close material-symbols-outlined"
+            onClick={() => setOpenModal(false)}
+          >
+            close
+          </span>
+        </div>
+        <p className="servicesmodal__text">{card.description}</p>
+        <p className="servicesmodal__subtitle">El servicio incluye:</p>
+        <div className="servicesmodal__subservices-container">
+          {card.subservices?.map((service) => (
+            <div className="servicesmodal__subservices" key={service}>
+              <p>{service}</p>
+            </div>
+          ))}
+        </div>
+        <a href="/#contacto">
+          <button className="servicesmodal__button">CONTACTAR</button>
+        </a>
+      </div>
+    </div>
+  );
+};
