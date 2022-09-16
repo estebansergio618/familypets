cd89f45104ac17f857b8dd0530e4fc2f7d70a43c
Matias Solis <solisma42@gmail.com>
2022-09-16 15:41:31 -0300

    added more team members

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -4,22 +4,34 @@ import Slider from "react-slick";
 import den from "../assets/images/denis.webp";
 import mar from "../assets/images/martin.webp";
 import wen from "../assets/images/wendy.webp";
+import personal1 from "../assets/images/personal1.webp";
+import personal2 from "../assets/images/personal2.webp";
+import personal3 from "../assets/images/personal3.webp";
+import personal4 from "../assets/images/personal4.webp";
+import personal5 from "../assets/images/personal5.webp";
 const Team = () => {
   const settings = {
     dots: false,
     infinite: true,
     speed: 1000,
-    slidesToShow: 3,
+    slidesToShow: 4,
     autoplay: true,
     autoplaySpeed: 4000,
     cssEase: "linear",
     arrows: false,
     responsive: [
       {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3,
+          dots: true,
+        },
+      },
+      {
         breakpoint: 1000,
         settings: {
           slidesToShow: 2,
-          dots:true,
+          dots: true,
         },
       },
       {
@@ -27,13 +39,23 @@ const Team = () => {
         settings: {
           slidesToShow: 1,
           speed: 1500,
-          dots:true,
+          dots: true,
           centerMode: true,
           centerPadding: "0",
         },
       },
     ],
   };
+  const personal:{name:string, role:string, image:string}[] = [
+    { name: "Denís", role: "Médica Veterinaria CEO", image: den },
+    { name: "Wendy", role: "Administración", image: wen },
+    { name: "Martín", role: "Médico Veterinario", image: mar },
+    { name: "Leslie", role: "Practicante Veterinaria", image: personal1 },
+    { name: "Josué Z.", role: "Grooming", image: personal2 },
+    { name: "Jonathan ", role: "Grooming", image: personal3 },
+    { name: "Luis", role: "Delivery", image: personal4 },
+    { name: "Lucila", role: "Counter", image: personal5 },
+  ];
   return (
     <section className="team" id="equipo">
       <div className="team__presentation">
@@ -45,48 +67,26 @@ const Team = () => {
       </div>
       <div className="team__card-container">
         <Slider {...settings}>
-          <div className="team__card">
-            <div className="team__card-up">
-              <div
-                className="team__card-image"
-                style={{ backgroundImage: `url(${den})` }}
-              />
+          {personal.map((person) => (
+            <div className="team__card" key={person.name + person.role}>
+              <div className="team__card-up">
+                <div
+                  className="team__card-image"
+                  style={{ backgroundImage: `url(${person.image})` }}
+                />
+              </div>
+              <div className="team__card-down">
+                <p className="team__card-name">{person.name}</p>
+                <p className="team__card-role">{person.role}</p>
+              </div>
             </div>
-            <div className="team__card-down">
-              <p className="team__card-name">Denís</p>
-              <p className="team__card-role">Médica Veterinaria CEO</p>
-            </div>
-          </div>
-          <div className="team__card">
-            <div className="team__card-up">
-              <div
-                className="team__card-image"
-                style={{ backgroundImage: `url(${wen})` }}
-              />
-            </div>
-            <div className="team__card-down">
-              <p className="team__card-name">Wendy</p>
-              <p className="team__card-role">Counter</p>
-            </div>
-          </div>
-          <div className="team__card">
-            <div className="team__card-up">
-              <div
-                className="team__card-image"
-                style={{ backgroundImage: `url(${mar})` }}
-              />
-            </div>
-            <div className="team__card-down">
-              <p className="team__card-name">Martín</p>
-              <p className="team__card-role">Médico Veterinario</p>
-            </div>
-          </div>
+          ))}
         </Slider>
       </div>
       <div className="team__contact">
         <p className="team__text">¿Quieres formar parte? ¡Contáctanos!</p>
         <HashLink to="/familypets#contacto">
-        <button className="team__button">CONTACTO</button>
+          <button className="team__button">CONTACTO</button>
         </HashLink>
       </div>
     </section>
